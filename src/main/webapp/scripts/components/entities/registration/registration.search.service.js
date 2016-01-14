d18266fe7dcd238b1ba34ade554cf7e7126ee0d2
dlwhitehurst <dlwhitehurst@me.com>
2016-01-14 12:22:37 -0500

    MUD-16 deleted registration (custom) entity

--- src/main/webapp/scripts/components/entities/registration/registration.search.service.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('mudskipperApp')
-    .factory('RegistrationSearch', function ($resource) {
-        return $resource('api/_search/registrations/:query', {}, {
-            'query': { method: 'GET', isArray: true}
-        });
-    });
