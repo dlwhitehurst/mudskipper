c449cae10a52acd321e398e9848e6d3f5d610e2a
dlwhitehurst <dlwhitehurst@me.com>
2016-01-14 23:53:08 -0500

    removed carets from entity name macro

--- delete-entity.sh
+++ delete-entity.sh
@@ -22,11 +22,11 @@ fi
 
 echo;
 
-rm -rf src/main/resources/config/liquibase/changelog/*_added_entity_${ENTITY_NAME^}.xml
-rm -rf src/main/java/${GROUP_ID}domain/${ENTITY_NAME^}.java
-rm -rf src/main/java/${GROUP_ID}repository/${ENTITY_NAME^}Repository.java
-rm -rf src/main/java/${GROUP_ID}repository/search/${ENTITY_NAME^}SearchRepository.java
-rm -rf src/main/java/${GROUP_ID}web/rest/${ENTITY_NAME^}Resource.java
+rm -rf src/main/resources/config/liquibase/changelog/*_added_entity_${ENTITY_NAME}.xml
+rm -rf src/main/java/${GROUP_ID}domain/${ENTITY_NAME}.java
+rm -rf src/main/java/${GROUP_ID}repository/${ENTITY_NAME}Repository.java
+rm -rf src/main/java/${GROUP_ID}repository/search/${ENTITY_NAME}SearchRepository.java
+rm -rf src/main/java/${GROUP_ID}web/rest/${ENTITY_NAME}Resource.java
 
 rm -rf src/main/webapp/scripts/app/entities/${ENTITY_NAME}/${ENTITY_NAME}.js
 rm -rf src/main/webapp/scripts/app/entities/${ENTITY_NAME}/${ENTITY_NAME}s.html
@@ -43,8 +43,8 @@ rm -rf src/main/webapp/scripts/components/entities/${ENTITY_NAME}/${ENTITY_NAME}
 rm -rf src/main/webapp/scripts/components/entities/${ENTITY_NAME}/${ENTITY_NAME}.search.service.js
 rm -rf src/main/webapp/scripts/components/entities/${ENTITY_NAME}
 
-rm -rf src/test/java/${GROUP_ID}web/rest/${ENTITY_NAME^}ResourceIntTest.java
-rm -rf src/test/gatling/simulations/${ENTITY_NAME^}GatlingTest.scala
+rm -rf src/test/java/${GROUP_ID}web/rest/${ENTITY_NAME}ResourceIntTest.java
+rm -rf src/test/gatling/simulations/${ENTITY_NAME}GatlingTest.scala
 rm -rf src/test/javascript/spec/app/entities/${ENTITY_NAME}/${ENTITY_NAME}-detail.controller.spec.js
 rm -rf src/test/javascript/spec/app/entities/${ENTITY_NAME}
 
@@ -52,8 +52,8 @@ rm -rf src/main/webapp/i18n/en/${ENTITY_NAME}.json
 rm -rf src/main/webapp/i18n/ru/${ENTITY_NAME}.json
 rm -rf src/main/webapp/i18n/uz/${ENTITY_NAME}.json
 
-rm -rf ./.jhipster/${ENTITY_NAME^}.json
+rm -rf ./.jhipster/${ENTITY_NAME}.json
 
 echo;
-echo "Deleting ${ENTITY_NAME^} is completed.";
+echo "Deleting ${ENTITY_NAME} is completed.";
 echo "################";
